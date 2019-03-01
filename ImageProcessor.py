ec5c860a96c944a62efd4142b08490cdb0d26efd
Matt Erickson <matterickson9819@gmail.com>
2019-02-28 19:47:09 -0600

    The image processing script now works

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,19 +1,29 @@
 import os, sys
 from preprocessor import resizeImg
 import cv2
-path = "./Data/Test/"
-
-newLocation = path + "resizedImages"
-
-if(os.path.isdir(newLocation == False)):
-    os.mkdir(newLocation)
-
-
-
+path = "./Data/words/"
+count = 0
 for(dirpath, dirnames, filenames) in os.walk(path):
     for files in filenames:
+        count= count+1
+        print("Files Resized:", count)
+        print(files)
         relative = os.path.join(dirpath,files)
-        print(relative)
-        Image = resizeImg(relative)
-        cv2.imshow("Img",Image)
-        cv2.waitKey(0)
+        resizeImg(relative)
+
+
+#I wrote the code below to check to see how many files were successful
+# Only two were skipped and one I did manually
+# path2 = "./Data/Resized/"
+# count2 =0
+# for(dirpath,dirnames, filenames) in os.walk(path):
+#     for files in filenames:
+#         count = count +1
+#
+# for(dirpath,dirnames,filenames) in os.walk(path2):
+#     for files in filenames:
+#         count2 = count2 + 1
+#
+#
+# print("Images in words directory:", count)
+# print("Images in Resized directory:", count2)
--- preprocessor.py
+++ preprocessor.py
@@ -1,23 +1,39 @@
 import cv2
-
+import os
 def resizeImg(path):
 
-    print(path)
-    image = cv2.imread(path)
-    print()
-    h = image.shape[0]
-    w = image.shape[1]
+    try:
+        print(path)
+        image = cv2.imread(path)
+        h = image.shape[0]
+        w = image.shape[1]
 
-    height = 32
+        height = 32
 
-    if w == None and h == None:
-        return image
-    else:
-        r = height / float(h)
-        dim = (int(w * r), height)
+        if w is None and h is None:
+            print("Damaged Image")
+        else:
+            r = height / float(h)
+            dim = (int(w * r), height)
 
-    resized = cv2.resize(image, dim, interpolation= cv2.INTER_AREA)
-    #return resized
-    cv2.imshow("img", resized)
-    cv2.waitKey(0)
+            #creates the resized image
+            resized = cv2.resize(image, dim, interpolation= cv2.INTER_AREA)
 
+            #gets the files extension
+            ext = os.path.splitext(path)[1]
+
+            # gets the files base name and takes the file extension off
+            baseName = os.path.basename(path)
+            baseName = os.path.splitext(baseName)[0]
+
+            # Sets the resized name
+            newName = baseName + "-Resized" + ext
+
+            newLocation = "./Data/Resized/" + newName
+            print(newLocation)
+            print()
+
+            cv2.imwrite(newLocation, resized)
+
+    except AttributeError:
+        print("Image couldnt be loaded")
