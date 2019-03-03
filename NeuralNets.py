85c77a1da91d0a620cf40afd0623f6dda4b7112d
Matt Erickson <matterickson9819@gmail.com>
2019-03-02 18:07:15 -0600

    More progress on CNN

--- NeuralNets.py
+++ NeuralNets.py
@@ -12,4 +12,20 @@ class NeuralNet:
         self.batchSize = batchSize
         self.learningRate = learningRate
         self.trainSession = trainSessions
-        
+
+        n_classes = 26
+
+        x = tf.placeholders("float", [None, 128,32,1])
+        y = tf.placeholders("float", [None, n_classes])
+
+
+    def conv2d(self,x,w,b,strides =1):
+        #Wrapped for bias and relu activation
+
+        x = tf.nn.conv2d(x,w,strides = [1,strides,strides,1], padding = "SAME")
+        y = tf.nn.bias_add(x,b)
+        return tf.nn.relu(x)
+
+    def maxpool2D(selfx, k=2):
+        return tf.nn.max_pool(x, ksize=[1,k,k,1], strides=[1,k,k,1],padding="SAME")
+    
