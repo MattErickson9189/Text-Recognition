3812517bef88c3c47b23d9de3275fb373dc635a9
Matt Erickson <matterickson9819@gmail.com>
2019-03-07 19:05:07 -0600

    Started work on the data loader

--- DataLoad.py
+++ DataLoad.py
@@ -25,4 +25,37 @@ wordsList = "./Data/words.txt"
 #             testCount +=1
 #             print(dest)
 
+class DataLoader:
+
+    def __init__(self, filePath, batchSize, textLength):
+
+        self.path = filePath
+        self.batchSize = batchSize
+        self.textLength = textLength
+
+        #store the images in the batch
+        self.images = []
+
+        #Loads the word list
+        list = open(wordsList)
+
+        chars = set()
+
+        for line in list:
+
+            if not line or line[0] =='#':
+                continue
+
+            lineSplit = line.strip().split(' ')
+            assert len(lineSplit) >= 9
+
+
+            #formats the file path to match the words.txt
+            fileNameSplit = lineSplit[0].split('-')
+            fileName = path + fileNameSplit[0]+ '/' + fileNameSplit[0] + '-' + fileNameSplit[1] + '/' + lineSplit[0] + '.png'
+
+            gtText = self.truncateLabel(' '.join(lineSplit[8:]), textLength)
+            chars = chars.union(set(list(gtText)))
+
+            self.samples.append(Sample)
 
