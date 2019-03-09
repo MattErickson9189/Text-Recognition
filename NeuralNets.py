1987e7e7edbccfb80917827f3d0fb7fcb07b9112
Matt Erickson <matterickson9819@gmail.com>
2019-03-09 12:19:59 -0600

    Made more progress on the NeuralNet Class

--- NeuralNets.py
+++ NeuralNets.py
@@ -49,7 +49,66 @@ class NeuralNet:
 
         self.cnnOut4d = pool
 
+    def CTC(self):
+        # BxTxC -> TxBxC
+        self.ctcIn3dTBC = tf.transpose(self.rnnOut3d, [1, 0, 2])
+        # ground truth text as sparse tensor
+        self.gtTexts = tf.SparseTensor(tf.placeholder(tf.int64, shape=[None, 2]), tf.placeholder(tf.int32, [None]),
+                                       tf.placeholder(tf.int64, [2]))
 
+        # calc loss for batch
+        self.seqLen = tf.placeholder(tf.int32, [None])
+        self.loss = tf.reduce_mean(
+            tf.nn.ctc_loss(labels=self.gtTexts, inputs=self.ctcIn3dTBC, sequence_length=self.seqLen,
+                           ctc_merge_repeated=True))
+
+        # calc loss for each element to compute label probability
+        self.savedCtcInput = tf.placeholder(tf.float32, shape=[Model.maxTextLen, None, len(self.charList) + 1])
+        self.lossPerElement = tf.nn.ctc_loss(labels=self.gtTexts, inputs=self.savedCtcInput,
+                                             sequence_length=self.seqLen, ctc_merge_repeated=True)
+
+        # decoder: either best path decoding or beam search decoding
+        if self.decoderType == DecoderType.BestPath:
+            self.decoder = tf.nn.ctc_greedy_decoder(inputs=self.ctcIn3dTBC, sequence_length=self.seqLen)
+        elif self.decoderType == DecoderType.BeamSearch:
+            self.decoder = tf.nn.ctc_beam_search_decoder(inputs=self.ctcIn3dTBC, sequence_length=self.seqLen,
+                                                         beam_width=50, merge_repeated=False)
+        elif self.decoderType == DecoderType.WordBeamSearch:
+            # import compiled word beam search operation (see https://github.com/githubharald/CTCWordBeamSearch)
+            word_beam_search_module = tf.load_op_library('TFWordBeamSearch.so')
+
+            # prepare information about language (dictionary, characters in dataset, characters forming words)
+            chars = str().join(self.list)
+            wordChars = open('../Data/Lists/wordCharList.txt').read().splitlines()[0]
+            corpus = open('../Data/Lists/corpus.txt').read()
+
+            # decode using the "Words" mode of word beam search
+            self.decoder = word_beam_search_module.word_beam_search(tf.nn.softmax(self.ctcIn3dTBC, dim=2), 50, 'Words',
+                                                                    0.0, corpus.encode('utf8'), chars.encode('utf8'),
+                                                                    wordChars.encode('utf8'))
+
+    def toSparse(self, text):
+        indices = []
+        values = []
+        shape = [len(text),0]
+
+        for(batchElement, text) in enumerate(text):
+
+            labelStr = [str.charList.index(c) for c in text]
+
+            if len(labelStr) > shape[1]:
+                shape[1]= len(labelStr)
+
+            for(i, label) in enumerate(labelStr):
+                indices.append([batchElement, i])
+                values.append(label)
+
+        return (indices,values,shape)
 
     def trainBatch(self, batch):
-        
+
+        numBatchOfElements = len(batch.imgs)
+        sparse = self.toSparse(batch.gtTexts)
+        #Decay the learning rate
+        rate = .01 if self.numTrained < 10 else (.001 if self.numTrained< 10000 else .00010)
+        evalList = [self.optimizer,]
