ca1a372df59d4f84cbc1011fe18a74cbbf17908f
Matt Erickson <matterickson9819@gmail.com>
2019-03-13 17:39:08 -0500

    Fixed some runtime erros

--- NeuralNets.py
+++ NeuralNets.py
@@ -26,10 +26,12 @@ class NeuralNet:
 
         self.inputImgs = tf.placeholder(tf.float32, shape=(None, NeuralNet.size[0], NeuralNet.size[1]))
 
-        self.CNN(self)
+        self.CNN()
+        self.CTC()
 
         self.numTrained = 0
-        self.learningRate = tf.placeholder(tf.float, shape=[])
+        self.learningRate = tf.placeholder(tf.float32, shape=[])
+
         self.update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         with tf.control_dependencies(self.update_ops):
             self.optimizer= tf.train.RMSPropOptimizer(self.learningRate).minimize(self.loss)
@@ -62,7 +64,7 @@ class NeuralNet:
 
         cnnIn = tf.expand_dims(input=self.inputImgs, axis=3)
 
-        kValues = [5,5,3,3]
+        kValues = [5,5,3,3,3]
         featureVals = [1,32,64,128,128,256]
         strideVals = poolVals = [(2, 2), (2, 2), (1, 2), (1, 2), (1, 2)]
         numLayers = len(strideVals)
@@ -71,13 +73,17 @@ class NeuralNet:
 
         for i in range(numLayers):
 
+            #print("kValue[i]: ", kValues[i], " featureVals[i]: ", featureVals[i], " featureVal[i+1]", featureVals[i+1])
             kernel = tf.Variable(tf.truncated_normal([kValues[i], kValues[i], featureVals[i], featureVals[i+1]], stddev=0.1))
             conv = tf.nn.conv2d(pool,kernel, padding="SAME", strides=(1,1,1,1))
-            conv_norm = tf.layers.batch_normalization(conv, training=self.is_train)
+            conv_norm = tf.layers.batch_normalization(conv, training=self.isTrain)
             relu = tf.nn.relu(conv_norm)
-            pool = tf.nn.max_pool(relu, (1, poolVals[i][0], poolVals[i][1], 1), (1, strideVals[i][0], strideVals[i][1],1), "VAILD")
+            pool = tf.nn.max_pool(relu, (1, poolVals[i][0], poolVals[i][1], 1), (1, strideVals[i][0], strideVals[i][1],1), "VALID")
 
         self.cnnOut4d = pool
+
+    def RNN(self):
+        
 
     def CTC(self):
         # BxTxC -> TxBxC
