8f5b5fbffa3df3aa0007f44a5a83efc0766dd5dd
Matt Erickson <matterickson9819@gmail.com>
2019-03-13 19:00:33 -0500

    Updated preprocessor so all images have unified width and height

--- preprocessor.py
+++ preprocessor.py
@@ -1,24 +1,44 @@
 import cv2
 import os
-
+import numpy as np
 def resizeImg(path):
 
     try:
         print(path)
-        image = cv2.imread(path)
+        image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
         h = image.shape[0]
         w = image.shape[1]
-
-        height = 32
 
         if w is None and h is None:
             print("Damaged Image")
         else:
-            r = height / float(h)
-            dim = (int(w * r), height)
+
+            desiredHeight = 32
+            desiredWidth = 128
+
+            fx = w / desiredWidth
+            fy = h / desiredHeight
+
+            f = max(fx, fy)
+            size = (max(min(desiredWidth, int(w / f)), 1), max(min(desiredHeight, int(h / f)), 1)) # scale according to f (result at least 1 and at most wt or ht)
 
             # creates the resized image
-            resized = cv2.resize(image, dim, interpolation= cv2.INTER_AREA)
+            resized = cv2.resize(image, size, )
+
+            target = np.ones([desiredHeight,desiredWidth]) * 255
+            target[0:size[1], 0:size[0]] = resized
+
+            img = cv2.transpose(target)
+
+
+            #normalize
+            (m,s) = cv2.meanStdDev(img)
+
+            m = m[0][0]
+            s = s[0][0]
+
+            img = img - m
+            img = img/s if s>0 else img
 
             # gets the files extension
             ext = os.path.splitext(path)[1]
@@ -34,7 +54,7 @@ def resizeImg(path):
             print(newLocation)
             print()
 
-            cv2.imwrite(newLocation, resized)
+            cv2.imwrite(newLocation, img)
 
     except AttributeError:
         print("Image couldnt be loaded")
