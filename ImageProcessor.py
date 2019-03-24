3fd61fc614e12d38296014c0db861cd02885914a
Matt Erickson <matterickson9819@gmail.com>
2019-03-24 12:53:15 -0500

    Minor changes

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -3,27 +3,27 @@ from preprocessor import resizeImg
 path = "./Data/words/"
 count = 0
 
-for(dirpath, dirnames, filenames) in os.walk(path):
-    for files in filenames:
-        count= count+1
-        print("Files Resized:", count)
-        print(files)
-        relative = os.path.join(dirpath,files)
-        resizeImg(relative)
+# for(dirpath, dirnames, filenames) in os.walk(path):
+#     for files in filenames:
+#         count= count+1
+#         print("Files Resized:", count)
+#         print(files)
+#         relative = os.path.join(dirpath,files)
+#         resizeImg(relative)
 
 
 #I wrote the code below to check to see how many files were successful
-# Only two were skipped and one I did manually
-# path2 = "./Data/Resized/"
-# count2 =0
-# for(dirpath,dirnames, filenames) in os.walk(path):
-#     for files in filenames:
-#         count = count +1
-#
-# for(dirpath,dirnames,filenames) in os.walk(path2):
-#     for files in filenames:
-#         count2 = count2 + 1
-#
-#
-# print("Images in words directory:", count)
-# print("Images in Resized directory:", count2)
+#Only two were skipped and one I did manually
+path2 = "./Data/Resized/"
+count2 =0
+for(dirpath,dirnames, filenames) in os.walk(path):
+    for files in filenames:
+        count = count +1
+
+for(dirpath,dirnames,filenames) in os.walk(path2):
+    for files in filenames:
+        count2 = count2 + 1
+
+
+print("Images in words directory:", count)
+print("Images in Resized directory:", count2)
